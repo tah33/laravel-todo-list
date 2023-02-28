2d6da64ca0e6767aad01d3f01c60efa7a553a8fe
Tanvir Ahmed Hera <tahmedhera@gmail.com>
2023-03-01 00:43:16 +0600

    first commit

---
+++ database/migrations/2023_02_28_184003_create_tasks_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('todo_list_id');
+            $table->foreign('todo_list_id')->references('id')->on('todo_lists')->onDelete('cascade');
+            $table->string('name');
+            $table->boolean('is_completed')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
